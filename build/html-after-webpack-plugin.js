008428e7270fe377d483655d7ef76f74c8743f40
niuDazhuang <dengwm666@icloud.com>
2018-06-14 20:09:52 +0800

    Update html-after-webpack-plugin.js

--- build/html-after-webpack-plugin.js
+++ build/html-after-webpack-plugin.js
@@ -3,13 +3,13 @@ const pluginName = 'HtmlAfterWebpackPlugin'
 class HtmlAfterWebpackPlugin {
   apply (compiler) {
     compiler.hooks.compilation.tap(pluginName, compilation => {
-      compilation.hooks.htmlWebpackPluginBeforeHtmlProcessing.tap(pluginName,htmlPluginData => {
+      compilation.hooks.htmlWebpackPluginBeforeHtmlProcessing.tap(pluginName, htmlPluginData => {
         const { html, assets } = htmlPluginData
         const {css, js} = assets
         htmlPluginData.html = html
           .replace("<!--injectcss-->", css.map(item => `<link rel="stylesheet" href="${item}"/>`).join(""))
           .replace("<!--injectjs-->", js.map(item => `<script src="${item}"></script>`).join(""))
-    })
+      })
     })
   }
 }
